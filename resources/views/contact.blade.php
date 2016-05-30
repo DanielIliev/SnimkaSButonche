8cc00edcbe1f6c1c62f9b64b7b440dafc6e392c0
DanielIlievDidiencho <daniel.iliev.didiencho@gmail.com>
2016-05-30 12:58:48 +0300

    Design added, views linked, basic structure for additional pages

--- resources/views/contact.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.Master')
-@section('title', 'Контакт')
-@section('content')
-	<div class="container">
-		<h3>Някакви разговори</h3>
-	</div>
-@endsection
--- resources/views/home.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.Master')
-@section('title', 'Домашна страница')
-@section('content')
-	<div class="container">
-		<h3>Добре дошли в главната страница</h3>
-		<h4>Картофена салата</h4>
-	</div>
-@endsection

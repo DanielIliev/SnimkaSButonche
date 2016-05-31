2ec14fe10e60e47c6221648b6dbaea262b14b4fc
DanielIlievDidiencho <daniel.iliev.didiencho@gmail.com>
2016-05-31 16:30:52 +0300

    Design Changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,22 @@
 @section('title', 'Домашна страница')
 @section('content')
 	<div class="container">
-		<h3>Добре дошли в главната страница</h3>
-		<h4>Картофена салата</h4>
+		<div class="jumbotron navbar">
+			<h2>Добре дошли в главната страница</h2>
+			<br>
+			<h3 class="randomJoke"></h3>
+		</div>
 	</div>
+	<script>
+		$(function() {
+			$.getJSON( "https://api.chucknorris.io/jokes/random", function( data ) {
+				$.each(data, function(key, val) {
+					if (key == "value") {
+						joke = val;
+						$('.randomJoke').text(joke);
+					}
+				});
+			});
+		});
+	</script>
 @endsection
